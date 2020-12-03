e53be68b72c328aedc9a12ff46462b254f0ddb73
gdelcast <airguillo@gmail.com>
2020-12-03 10:24:42 -0500

    .

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,6 +5,7 @@ import { IonGrid, IonRow, IonCol } from '@ionic/react';
 import { personCircle } from "ionicons/icons";
 import { useHistory } from "react-router-dom";
 import { IonItem, IonLabel, IonInput, IonButton, IonIcon, IonAlert } from '@ionic/react';
+import {HTTP} from '@ionic-native/http';
 
 function validateEmail(email: string) {
 //  const re = /^((?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]))$/;
@@ -43,7 +44,11 @@ const Login: React.FC = () => {
     }
 
 /*
-    const api = axios.create({
+//==========================
+// HTTP CALL USING AXIOS
+//==========================
+
+      const api = axios.create({
 //      baseURL: `https://reqres.in/api`
       headers: { 
         'Access-Control-Allow-Origin': '*',
@@ -72,17 +77,20 @@ const Login: React.FC = () => {
          })
          */
 
-        
+//==========================
+// HTTP CALL USING FETCH
+//==========================
+        /*
         //fetch('http://localhost:5000/jderest/v2/tokenrequest', {
         fetch('http://10.1.4.9:8180/jderest/v2/tokenrequest', {
           method: 'POST',
           headers: {
                    'Access-Control-Allow-Origin': '*',
                    'Content-Type': 'application/json',
-                   /*'Access-Control-Allow-Credentials': 'true',
-                    'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,OPTIONS',
-                    'Access-Control-Allow-Headers' : 'Origin, Content-Type, Accept, Authorization,X-Requested-With',
-                   mode:'no-cors'*/
+                   //'Access-Control-Allow-Credentials': 'true',
+                   // 'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,OPTIONS',
+                   // 'Access-Control-Allow-Headers' : 'Origin, Content-Type, Accept, Authorization,X-Requested-With',
+                   //mode:'no-cors'
           },
 
          body: JSON.stringify(loginData)
@@ -97,8 +105,29 @@ const Login: React.FC = () => {
                   setMessage('Failed to connect to AIS ' + error);
                   setIserror(true);
         });
-        
+        */
 
+//==========================
+// HTTP CALL USING IONIC NATIVE
+//==========================
+
+    HTTP.setServerTrustMode('default');
+    HTTP.setDataSerializer('json');
+    HTTP.setHeader('','Access-Control-Allow-Origin','*');
+    HTTP.setHeader('','Content-Type','application/json');
+    
+
+    HTTP.post('http://10.1.4.9:8180/jderest/v2/tokenrequest', loginData, {})
+    .then((responseJson) => {
+      console.log(responseJson);
+      setMessage(JSON.parse(responseJson.data).userInfo.token);
+      setIserror(true);
+    })
+    .catch(error => {
+        console.error('Error: ', error);
+        setMessage('Failed to connect to AISxxx ' + error);
+        setIserror(true);
+    });
 
   };
 
